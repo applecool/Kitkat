95e04fc9e19f9d8c05a8592cfbf20a2f8de5d929
applecool <kmitharsha@gmail.com>
2015-05-16 22:32:38 -0500

    Added firstApp

---
+++ MyFirstApp/src/org/shellzero/myfirstapp/MainActivity.java
@@ -0,0 +1,39 @@
+package org.shellzero.myfirstapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Toast toastMessage = Toast.makeText(this,"My app has successfully loaded", Toast.LENGTH_LONG );
+		toastMessage.show();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			Toast settingsMessage = Toast.makeText(this, "Settings button clicked", Toast.LENGTH_LONG);
+			settingsMessage.show();
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
