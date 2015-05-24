39aba12fff2a4d0b043ed08ed76667314dc990b5
applecool <kmitharsha@gmail.com>
2015-05-23 23:48:45 -0500

    Added Calculator

---
+++ Calculator/src/org/shellzero/calculator/MainActivity.java
@@ -0,0 +1,135 @@
+package org.shellzero.calculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+	EditText operand1;
+	EditText operand2;
+
+	Button btnAdd;
+	Button btnSubtract;
+	Button btnMultiply;
+	Button btnDivide;
+	Button btnClear;
+
+	TextView result;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		// Operand fields from the main screen
+				operand1 = (EditText) findViewById(R.id.editOperand1);
+				operand2 = (EditText) findViewById(R.id.editOperand2);
+
+				// Associate buttons
+				btnAdd = (Button) findViewById(R.id.btnAdd);
+				btnSubtract = (Button) findViewById(R.id.btnSubtract);
+				btnMultiply = (Button) findViewById(R.id.btnMultiply);
+				btnDivide = (Button) findViewById(R.id.btnDivide);
+				btnClear = (Button) findViewById(R.id.btnClear);
+
+				// Associate result textfield
+				result = (TextView) findViewById(R.id.textResult);
+
+				// Add functionality
+				btnAdd.setOnClickListener(new View.OnClickListener() {
+
+					@Override
+					public void onClick(View v) {
+						// TODO Auto-generated method stub
+						float oper1 = Float.parseFloat(operand1.getText().toString());
+						float oper2 = Float.parseFloat(operand2.getText().toString());
+
+						float theResult = oper1 + oper2;
+
+						result.setText(Float.toString(theResult));
+					}
+				});
+
+				// Subtract functionality
+				btnSubtract.setOnClickListener(new View.OnClickListener() {
+
+					@Override
+					public void onClick(View v) {
+						// TODO Auto-generated method stub
+						float oper1 = Float.parseFloat(operand1.getText().toString());
+						float oper2 = Float.parseFloat(operand2.getText().toString());
+
+						float theResult = oper1 - oper2;
+
+						result.setText(Float.toString(theResult));
+					}
+				});
+
+				// Multiply functionality
+				btnMultiply.setOnClickListener(new View.OnClickListener() {
+
+					@Override
+					public void onClick(View v) {
+						// TODO Auto-generated method stub
+						float oper1 = Float.parseFloat(operand1.getText().toString());
+						float oper2 = Float.parseFloat(operand2.getText().toString());
+
+						float theResult = oper1 * oper2;
+
+						result.setText(Float.toString(theResult));
+					}
+				});
+
+				// Divide functionality
+				btnDivide.setOnClickListener(new View.OnClickListener() {
+
+					@Override
+					public void onClick(View v) {
+						// TODO Auto-generated method stub
+						float oper1 = Float.parseFloat(operand1.getText().toString());
+						float oper2 = Float.parseFloat(operand2.getText().toString());
+
+						float theResult = oper1 / oper2;
+
+						result.setText(Float.toString(theResult));
+					}
+				});
+				
+				// Clear functionality
+				btnClear.setOnClickListener(new View.OnClickListener() {
+
+					@Override
+					public void onClick(View v) {
+						// TODO Auto-generated method stub
+						operand1.setText("");
+						operand2.setText("");
+						result.setText("0.00");
+					}
+				});
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
